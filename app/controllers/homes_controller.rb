70651d52428487f8e76b0a9e8fb9ef087010e94b
yaaman1002 <166231007+yaaman1002@users.noreply.github.com>
2024-07-17 13:22:24 +0000

    [update]レイアウト、トップページ

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,14 @@
 class HomesController < ApplicationController
   def top
+    @new_posts=Post.order(created_at: :desc).limit(1)
   end
   
   def about
   end
   
+  private
+  
+  def homes_params
+    params.require(:home).permit(:name, :profile_image, :introduction)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,15 @@
 class Post < ApplicationRecord
-  
+
   has_many_attached :images
   belongs_to :user
   has_many :liked_posts, dependent: :destroy
   has_many :comments, dependent: :destroy
-  
+
   def get_image
-    unless image.attached?
-      file_path = Rails.root.join('app/assets/images/default_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    unless images.first
+      file_path = Rails.root.join('app/assets/images/default-image.jpg')
+      images.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    images.first
   end
 end
