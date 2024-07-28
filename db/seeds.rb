cd1b8d916b6bf6c29c9e079a48e7e8ed2d54fc0c
yaaman1002 <166231007+yaaman1002@users.noreply.github.com>
2024-07-28 05:21:06 +0000

    [add]テストコード

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+olivia = User.find_or_create_by!(email: "olivia@example.com") do |user|
+  user.name = "Olivia"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg.jpeg"), filename:"sample-user1.jpg.jpeg")
+end
+
+james = User.find_or_create_by!(email: "james@example.com") do |user|
+  user.name = "James"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg.jpeg"), filename:"sample-user2.jpg.jpeg")
+end
+
+lucas = User.find_or_create_by!(email: "lucas@example.com") do |user|
+  user.name = "Lucas"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpeg"), filename:"sample-user3.jpg")
+end
+
+Post.find_or_create_by!(title: "初バレーボール観戦") do |one_post|
+  one_post.images=ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg")
+  one_post.body="楽しかった！"
+  one_post.user="olivia"
+end
+
+Post.find_or_create_by!(title: "ウルフドックス名古屋") do |one_post|
+  one_post.images=ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg")
+  one_post.body="大接戦の試合でした！"
+  one_post.user="James"
+end
+
+Post.find_or_create_by!(title: "オリンピック") do |one_post|
+  one_post.images=ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg")
+  one_post.body="ナイスゲームだったけど悔しい"
+  one_post.user="lucas"
+end
