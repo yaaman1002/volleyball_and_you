af99e16bc9557d98277974dfef38ecee43414c4a
yaaman1002 <166231007+yaaman1002@users.noreply.github.com>
2024-08-06 12:13:33 +0000

    [bugfix]user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :liked_posts, dependent: :destroy
-  has_many :post_images, dependent: :destroy
-  
+
   has_one_attached :profile_image
-         
+
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password=SecureRandom.urlsafe_base64
     end
   end
-  
+
   def get_profile_image(width,height)
     unless profile_image.attached?
       file_path=Rails.root.join('app/assets/images/default-image.jpg')
@@ -24,11 +23,11 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
   def liked_posted_by?(post_id)
     liked_post.where(post_id: post_id).exists?
   end
-  
+
   def self.search_for(content, method)
     if method == 'perfect'
       User.where(name: content)
@@ -40,7 +39,7 @@ class User < ApplicationRecord
       User.where('name LIKE ?', '%' + content + '%')
     end
   end
-  
+
   def active_for_authentication?
     super && (is_active == true)
   end
