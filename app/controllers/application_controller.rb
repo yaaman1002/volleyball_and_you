879a4ffdf41e021ff81e43d11dbc43d0559095b1
yaaman1002 <166231007+yaaman1002@users.noreply.github.com>
2024-08-13 12:43:51 +0000

    [update]routes.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   protected
-  
+
   def configure_authentication
     if admin_controller?
       authenticate_admin!
@@ -9,11 +9,11 @@ class ApplicationController < ActionController::Base
       authenticate_user! unless action_is_public?
     end
   end
-  
+
   def admin_controller?
     self.class.module_parent_name == 'Admin'
   end
-  
+
   def action_is_public?
     controller_name == 'homes' && action_name == 'top'
   end
--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -3,4 +3,8 @@ class Public::RegistrationsController < Devise::RegistrationsController
   def new
     super
   end
+  
+  def after_sign_in_path_for(resource)
+    posts_path
+  end
 end
